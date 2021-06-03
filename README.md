334f7796e43b57cd01889a52e85fc586faec6a99
Nathan Nguyen <nathanknguyen96@gmail.com>
2021-06-02 17:11:08 -0700

    readme update and code to generate password

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # PasswordGenerator
-This is a password generator that takes user input and generates a random password based on the criteria given by the user.
 
-Here is a screenshot on the application and here is a link to the deployed application.
+## About
+This is a password generator that takes user input and generates a random password based on the criteria given by the user. 
+
+## Links
+Here is a screenshot of the application and here is a link to the deployed application.
+
--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,29 @@
 // Assignment Code
+console.log(this);
 var generateBtn = document.querySelector("#generate");
-//console.log(document);
-console.log(generateBtn);
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  window.alert("Now ");
 
 }
 
+function generatePassword() {
+  var alphabet = "abcdefghijklmnopqrstuvwxyz";
+  var password = "";
+  var passwordLength = window.prompt("Choose a number between 8 and 128. That will be the length of your password");
+  var characterTypes = window.prompt("Now choose the special character that you want included in your random password. Choose between ");
+  var 
+  for (var i = 0; i < passwordLength - 1; i++) {
+    password += alphabet[Math.floor(Math.random() * alphabet.length)];
+  }
+
+  return password;
+
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
